c5f2068bc0e58a25375362ec931dd11c059a4227
Aleksandr Shmaraiev <ashmaraev@codenvy.com>
2016-06-26 13:29:20 +0300

    init

---
+++ src/com/codenvy/MainClass.java
@@ -0,0 +1,14 @@
+package com.codenvy;
+
+import org.mockito.Mockito;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+	// write your code here
+	String string = "Keep calm and enjoy coding with Codenvy!";
+	System.out.println(string);
+	Mockito mockito = new Mockito();
+	
+    }
+}
